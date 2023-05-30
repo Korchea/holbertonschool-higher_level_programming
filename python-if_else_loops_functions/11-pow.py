8933afd543d90c7b56db429b284d710638b4c6a7
Korchea <6334@holbertonstudents.com>
2023-05-30 10:46:24 -0700

    Task 11 completed

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def pow(a, b):
+    if b == 0:
+        return (1)
+    if b < 0:
+        a = 1 / a
+        b = -b
+    x = a
+    for i in range(1, b):
+        if a < 0:
+            a = -a * x
+        else:
+            a = a * x
+    return (a)
