d9c86025f87f0498d85e3ca2bf552cb2d28ae8a9
Korchea <6334@holbertonstudents.com>
2023-05-31 04:52:55 -0700

    Task 11 solved

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,16 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    if b == 0:
-        return (1)
-    if b < 0:
-        a = 1 / a
-        b = -b
-    x = a
-    for i in range(1, b):
-        if a < 0:
-            a = -a
-            a = a * x
-            a = -a
-        else:
-            a = a * x
-    return (a)
+    return (a**b)
