e9c284481eac466297f01a0ce61e7fca14795503
Korchea <6334@holbertonstudents.com>
2023-05-31 13:55:24 -0700

    4 Not works but it is something

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import hidden_4
+    i = 0
+    arr = []
+    for i in range(len(dir(hidden_4))):
+        if dir(hidden_4)[i][0] != '_' and dir(hidden_4)[i][1] != '_':
+            arr.append(dir(hidden_4)[i])
+    print(arr)
