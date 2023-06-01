5ca7238cdbbb9ed521017bde044dc89b0e520aa0
Korchea <6334@holbertonstudents.com>
2023-06-01 07:47:33 -0700

    Task 6 completed

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     length = len(argv)
     if length != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit (1)
+        exit(1)
     a = int(argv[1])
     b = int(argv[3])
     if argv[2] == "+":
@@ -18,4 +18,4 @@ if __name__ == "__main__":
         print("{} / {} = {}".format(a, b, c.div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit (1)
+        exit(1)
