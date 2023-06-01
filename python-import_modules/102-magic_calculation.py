18ee8840b220f5cd40930b1c9cfc73932b8cf52b
Korchea <6334@holbertonstudents.com>
2023-06-01 13:33:27 -0700

    Task 8 completed

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     def magic_calculation(a, b):
-    add = magic_calculation_102.add
-    sub = magic_calculation_102.sub
+        add = magic_calculation_102.add
+        sub = magic_calculation_102.sub
 
-    if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return c
-    else:
-        return sub(a, b)
+        if a < b:
+            c = add(a, b)
+            for i in range(4, 6):
+                c = add(c, i)
+            return c
+        else:
+            return sub(a, b)
