1f717d41cf61c13252d9bee4c71de27e93c6a2a3
Korchea <6334@holbertonstudents.com>
2023-06-05 06:51:29 -0700

    Task 4 completed

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    my_list_aux = []
+    for i in range(len(my_list)):
+        if idx >= 0 and idx < len(my_list):
+            my_list_aux.append(element)
+        else:
+            my_list_aux.append(my_list[i])
+    return my_list_aux
