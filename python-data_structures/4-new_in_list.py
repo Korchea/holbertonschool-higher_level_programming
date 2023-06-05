6c4ef64686c34cc7c25eaa33419ea9c0ba2c4bac
Korchea <6334@holbertonstudents.com>
2023-06-05 06:54:21 -0700

    Task 4 solved

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     my_list_aux = []
     for i in range(len(my_list)):
-        if idx >= 0 and idx < len(my_list):
+        if idx >= 0 and idx < len(my_list) and i == idx:
             my_list_aux.append(element)
         else:
             my_list_aux.append(my_list[i])
