d3037ab8e799c1ba835d79b48151538a5a9b7ccb
Korchea <6334@holbertonstudents.com>
2023-06-06 13:43:55 -0700

    Task 12 completed

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    number = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    sum = 0
+    if len(roman_string) == 1:
+        sum = number[roman_string[0]]
+    for i in range(1, len(roman_string)):
+        for key, value in number.items():
+            if roman_string[i] == key:
+                for pre_key, pre_value in number.items():
+                    if roman_string[i - 1] == pre_key and pre_value >= value:
+                        if i == 1:
+                            sum += value + pre_value
+                        else:
+                            sum += value
+                        break
+                    elif roman_string[i - 1] == pre_key and pre_value < value:
+                        sum += (value - pre_value)
+                        break
+                break
+    return sum
