307b10ffbfc920a6627cebeb807afd1f65b93770
Korchea <6334@holbertonstudents.com>
2023-06-07 09:17:03 -0700

    Task 12 solved

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,7 @@
 def roman_to_int(roman_string):
     number = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     sum = 0
+    flag = False
     if len(roman_string) == 1:
         sum = number[roman_string[0]]
     for i in range(1, len(roman_string)):
@@ -13,9 +14,14 @@ def roman_to_int(roman_string):
                             sum += value + pre_value
                         else:
                             sum += value
+                        flag = True
                         break
                     elif roman_string[i - 1] == pre_key and pre_value < value:
-                        sum += (value - pre_value)
+                        if flag:
+                            sum += (value - 2*pre_value)
+                        else:
+                            sum += (value - pre_value)
+                        flag = False
                         break
                     elif pre_key == 'M':
                         return 0
