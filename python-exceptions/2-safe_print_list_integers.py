8dc1a6df33054d7bfbebfbad7936dae53319e24c
Korchea <6334@holbertonstudents.com>
2023-06-08 05:30:36 -0700

    Task 0 and 1 solved, add 2 incompleted

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
+def safe_print_list_integers(my_list=[], x=0):
     np = 0
     for i in range(x):
         try:
-            print(my_list[i], end="")
+            print("{:d}".format(my_list[i]))
             np += 1
         except Exception:
             print()
