33823d453990e3dfae582f0aa7febb17d96dacdd
Korchea <6334@holbertonstudents.com>
2023-06-08 09:28:22 -0700

    Task 12 full solved

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     number = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     sum = 0
     flag = False
-    if not roman_string:
+    if not roman_string or type(roman_string) != str:
         return 0
     if len(roman_string) == 1:
         sum = number[roman_string[0]]
