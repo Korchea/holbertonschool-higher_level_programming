5fb53c8d1007872952513150ac7f0d8e3243af86
Korchea <6334@holbertonstudents.com>
2023-06-12 06:12:54 -0700

    Task 1 completed

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+In this file I create a class squere
+"""
+
+
+class Square:
+    """
+    Is a class Squere.
+    """
+    def __init__(self, size):
+        """
+        Define the size of the class
+        """
+        self.__size = size
