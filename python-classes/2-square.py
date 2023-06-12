68ee67bb769b1d02c53bc4e621e01c59a49d1030
Korchea <6334@holbertonstudents.com>
2023-06-12 08:33:52 -0700

    Task 2 completed

---
+++ python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+In this file I create a class squere
+"""
+
+
+class Square:
+    """
+    Is a class Squere.
+    """
+
+    def __init__(self, size=0):
+        """
+        Define the size of the class
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
