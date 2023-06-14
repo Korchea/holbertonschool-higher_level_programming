4c2b8ad46811ba55e78197d0b45f88cfa0d95cdf
Korchea <6334@holbertonstudents.com>
2023-06-14 11:50:43 -0700

    Task 4 solved

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
+"""print_square
+"""
+
+
 def print_square(size):
+    """print_square
+
+    Args:
+        size (int): Is an integer
+
+    Raises:
+        TypeError: size must be an integer
+        TypeError: size must be an integer
+        ValueError: size must be >= 0
+    """
     if type(size) == float and size < 0:
         raise TypeError("size must be an integer")
     if type(size) != int:
