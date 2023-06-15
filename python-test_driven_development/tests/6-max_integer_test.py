0f17a630de81bcb5c2ae63708c21306ac73a0a21
Korchea <6334@holbertonstudents.com>
2023-06-15 06:25:00 -0700

    Task 6 completed

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class max_integer_test(unittest.TestCase):
+
+    def standar_case(self):
+        self.assertEqual('[1, 2, 4, 3]'.max_integer(), '4')
+
+    def none_case(self):
+        self.assertEqual('[None]'.max_integer(), 'None')
+
+    def empty_case(self):
+        self.assertEqual(''.max_integer(), 'None')
+
+    def negative_case(self):
+        self.assertEqual('[-1]'.max_integer(), '-1')
+
+    def string_case(self):
+        self.assertEqual('["Hi"]'.max_integer(), 'Hi')
+
+
+if __name__ == '__main__':
+    unittest.main()
