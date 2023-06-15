53b649e5bd9c4ec1d9e6836b7e7aa06a85f00a00
Korchea <6334@holbertonstudents.com>
2023-06-15 07:12:16 -0700

    Task 6 solved

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -4,15 +4,16 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
-    """max_integer_test
+    """TestMaxInteger
 
     Args:
         unittest: Just cases to tests
     """
 
     def standar_case(self):
-        self.assertEqual('[1, 2, 4, 3]'.max_integer(), '4')
+        self.assertAlmostEqual(max_integer([1, 2, 4, 3]), 4)
 
     def none_case(self):
         self.assertEqual('[None]'.max_integer(), 'None')
