88039ab7572809ea6d9f06159c14ebfbdff05310
Korchea <6334@holbertonstudents.com>
2023-06-16 14:01:43 -0700

    Task 6 resolved

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -13,20 +13,23 @@ class TestMaxInteger(unittest.TestCase):
         unittest: Just cases to tests
     """
 
-    def test_standar(self):
+    def test_order(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_disorder(self):
         self.assertEqual(max_integer([1, 2, 4, 3]), 4)
 
     def test_none(self):
         self.assertEqual(max_integer([None]), None)
 
     def test_empty(self):
-        self.assertEqual(max_integer(), None)
+        self.assertEqual(max_integer([]), None)
 
     def test_negative(self):
-        self.assertEqual(max_integer([-1]), -1)
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
 
     def test_float(self):
-        self.assertEqual(max_integer([1.2]), 1.2)
+        self.assertEqual(max_integer([1.2, 1, 0]), 1.2)
 
     def test_only_zero(self):
         self.assertEqual(max_integer([0, 0]), 0)
