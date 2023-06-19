fe8945e8800f5893aa35c949bb8ef9a867839d6d
Korchea <6334@holbertonstudents.com>
2023-06-19 13:10:16 -0700

    Task 2 completed

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Crete a function is_same_class
+"""
+
+
+def is_same_class(obj, a_class):
+    """is_same_class
+
+    Args:
+        obj (subclass): Is an objet to compare
+        a_class (subclass): Is to compare
+
+    Returns:
+        Bool: If is an instance return True else False
+    """
+    return isinstance(obj, a_class)
