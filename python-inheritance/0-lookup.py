7b4f62a1bfbec172c3f84dae011cf7badfe92e5a
Korchea <6334@holbertonstudents.com>
2023-06-19 11:09:54 -0700

    Task 0 doc added

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+"""Function declare
+"""
+
+
 def lookup(obj):
+    """lookup
+
+    Args:
+        obj (subclass): Is an objet from a class
+
+    Returns:
+        list: Return the list of available attributes and methods of an object
+    """
     return dir(obj)
