fb24e901a396f79e33b5a1ed38b6a655125abac8
Korchea <6334@holbertonstudents.com>
2023-06-22 11:42:46 -0700

    Task 10 completed

---
+++ python-input_output/10-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Student
+
+    Returns:
+        JSON: Return a JSON class
+"""
+
+
+class Student():
+    """Student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.age = age
+        self.last_name = last_name
+        self.first_name = first_name
+
+    def to_json(self, attrs=None):
+        if attrs == None:
+            return self.__dict__
+        else:
+            aux = {}
+            for a in attrs:
+                if a == "first_name":
+                    aux.update({a: self.first_name})
+                elif a == "last_name":
+                    aux.update({a: self.last_name})
+                elif a == "age":
+                    aux.update({a: self.age})
+            myKeys = list(aux.keys())
+            myKeys.sort()
+            sorted_dict = {i: aux[i] for i in myKeys}
+            return sorted_dict
