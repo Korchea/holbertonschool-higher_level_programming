722c90bcc72349ff9ab6165969dbaf0c3fa4e6ae
Korchea <6334@holbertonstudents.com>
2023-06-22 09:31:07 -0700

    Task 9 completed

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""Student
+
+    Returns:
+        JSON: Return a JSON class
+"""
+
 
 class Student():
+    """Student
+    """
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
