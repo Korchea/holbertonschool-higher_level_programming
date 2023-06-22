0eb1c74e20735902ed7449c6094e4d3721e13cad
Korchea <6334@holbertonstudents.com>
2023-06-22 11:47:06 -0700

    Task 10 completed

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -16,7 +16,7 @@ class Student():
         self.first_name = first_name
 
     def to_json(self, attrs=None):
-        if attrs == None:
+        if attrs is None:
             return self.__dict__
         else:
             aux = {}
