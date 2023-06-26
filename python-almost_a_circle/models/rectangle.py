130447db0fea190f549d98dac6a196d73bd62692
Korchea <6334@holbertonstudents.com>
2023-06-26 07:23:59 -0700

    Task 2 completed

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Rectangle
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Rectangule
+
+    Args:
+        Base (class): Is just a class
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        return self.width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
