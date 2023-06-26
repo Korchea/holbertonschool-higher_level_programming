8243e9c4797103591474ae0b5ee5cc9479c05320
Korchea <6334@holbertonstudents.com>
2023-06-26 06:31:11 -0700

    Task 1 completed

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Base
+"""
+
+
+class Base():
+    """Base
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if not id:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
