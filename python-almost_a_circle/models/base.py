ca3b53c526856c5ec3668544aab9ba7348f7c61b
Korchea <6334@holbertonstudents.com>
2023-06-27 12:34:08 -0700

    Task 15 solved

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -25,7 +25,7 @@ class Base():
         Returns:
             dict
         """
-        if not list_dictionaries:
+        if not list_dictionaries or list_dictionaries == []:
             return []
         else:
             return json.dumps(list_dictionaries)
