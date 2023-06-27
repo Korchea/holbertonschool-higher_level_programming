7bf3a80a67de8826033e8904d9778fefabe9c862
Korchea <6334@holbertonstudents.com>
2023-06-27 07:05:53 -0700

    Task 9 completed

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -91,16 +91,29 @@ class Rectangle(Base):
                                                        self.__y, self.__width,
                                                        self.__height)
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """update
         """
-        if len(args) > 0:
-            super().__init__(args[0])
-        if len(args) > 1:
-            self.width = args[1]
-        if len(args) > 2:
-            self.height = args[2]
-        if len(args) > 3:
-            self.x = args[3]
-        if len(args) > 4:
-            self.y = args[4]
+        if args:
+            if len(args) > 0:
+                super().__init__(args[0])
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.height = args[2]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
+        if kwargs:
+            for key, value in sorted(kwargs.items()):
+                if key == "id":
+                    super().__init__(value)
+                if key == "width":
+                    self.width = value
+                if key == "height":
+                    self.height = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
