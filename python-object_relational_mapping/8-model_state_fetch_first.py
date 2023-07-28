28880de533b2319b00df89536a7e14a1cc5e359f
Korchea <6334@holbertonstudents.com>
2023-07-28 13:01:08 -0700

    Task 8 completed

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" This script that prints the first State object from the database hbtn_0e_6_usa.
+"""
+from sys import argv
+from sqlalchemy import create_engine
+import sqlalchemy as db
+from model_state import Base, State
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(argv[1], argv[2], argv[3]))
+
+    connection = engine.connect()
+    states = db.select(State.id, State.name).order_by(State.id)
+    result = connection.execute(states).fetchall()
+
+    if result is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(result[0][0], result[0][1]))
