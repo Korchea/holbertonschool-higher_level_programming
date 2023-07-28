2427d7e4e58d17691d267b89bdc4b1490a95fc0f
Korchea <6334@holbertonstudents.com>
2023-07-28 13:02:31 -0700

    Task 8 solved

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" This script that prints the first State object from the database hbtn_0e_6_usa.
+""" This script that prints the first State object from the database
+    hbtn_0e_6_usa.
 """
 from sys import argv
 from sqlalchemy import create_engine
