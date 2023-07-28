b23cc55073e020f5744797cc8d2faef57c6fff51
Korchea <6334@holbertonstudents.com>
2023-07-28 10:13:19 -0700

    Task 6 completed

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -2,21 +2,13 @@
 """ This python file that contains the class definition of a State and an
     instance Base = declarative_base().
 """
-from sys import argv
-import MySQLdb
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-if __name__ == '__main__':
-    cities = ""
-    sql = """SELECT cities.name, states.name
-    FROM cities
-    JOIN states ON cities.state_id = states.id;"""
-    db = MySQLdb.connect(user=argv[1], password=argv[2], db=argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute(sql)
-    states = cur.fetchall()
-    for i in states:
-        if i[1] == argv[4]:
-            if cities != "":
-                cities += ", "
-            cities += i[0]
-    print(cities)
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
